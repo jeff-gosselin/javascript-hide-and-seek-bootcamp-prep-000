b57447f078c37d7503c3725e74f27b3fd39ceb28
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-08-20 18:00:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var lis = document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var divs = document.getElementById("grand-node");
+  var parentDiv;
+  while (divs && divs.children[0]) {
+    parentDiv = divs;
+    divs = divs.children[0];
+  }
+  return divs;
+}
+// 2 variables tracking the the parent and the child, only returning the parent
