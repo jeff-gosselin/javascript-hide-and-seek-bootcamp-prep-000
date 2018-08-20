d22d86ed963255f0d058f653b79f25282fe01102
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-08-20 16:09:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var lis = document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var divs = document.getElementById("div#grand-node");
+  while (divs && divs.children[0]) {
+    var parentDiv = divs;
+    divs = divs.children[0];
+  }
+  return parentDiv;
+}
+// 2 variables tracking the the parent and the child, only returning the parent
